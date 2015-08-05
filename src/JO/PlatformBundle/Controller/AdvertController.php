b81ccb61426011f018831a4bf45957c9357e2847
Joseph <joseph.mesny@gmail.com>
2015-08-05 11:07:10 +0200

    Add advert controller method,  update index view

--- src/JO/PlatformBundle/Controller/AdvertController.php
+++ src/JO/PlatformBundle/Controller/AdvertController.php
@@ -5,29 +5,13 @@
 namespace JO\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpFoundation\Request;
 
 class AdvertController extends Controller
 {
-	public function addAction(Request $request)
-	{
-	  $session = $request->getSession();
-	   
-	  // Bien sûr, cette méthode devra réellement ajouter l'annonce
-	   
-	  // Mais faisons comme si c'était le cas
-	  $session->getFlashBag()->add('info', 'Annonce bien enregistrée');
-
-	  // Le « flashBag » est ce qui contient les messages flash dans la session
-	  // Il peut bien sûr contenir plusieurs messages :
-	  $session->getFlashBag()->add('info', 'Oui oui, il est bien enregistré !');
-
-	  // Puis on redirige vers la page de visualisation de cette annonce
-	  return $this->redirect($this->generateUrl('jo_platform_view', array('id' => 5)));
-	}
-
-    public function viewAction($id, Request $request)
+	// récupération de l'annonce correspondant a l'id
+    public function viewAction($id)
 	{
 		return $this->render('JOPlatformBundle:Advert:view.html.twig', array('id' => $id));
 	}
@@ -38,11 +22,42 @@ class AdvertController extends Controller
         return new Response("On pourrait afficher l'annonce correspondant au slug '".$slug."', créée en ".$year." et au format ".$_format.".");
     }
 	
+	// Récupération de la liste des annonces puis envois template
 	public function indexAction($page)
     {
-        $url = $this->get('router')->generate('jo_platform_view', array('id'=>5));
-		return new Response("L'url de l'annonce d'id 5 est : ".$url);
+        // throw error in case of bad page number.
+		if ($page <1)
+			throw new NotFoundHttpException('Page "'.$page.'" inexistante.');
+		
+		// TODO Return announce list
+		return $this->render('JOPlatformBundle:Advert:index.html.twig');
     }
+	
+	// gestion du formulaire d'ajout d'annonces
+	public function addAction(Request $request)
+	{
+		if ($request->isMethod('POST')){
+			$request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+			return $this->redirect($this->generateUrl('jo_platform_view', array('id' => 5)));
+		}
+		return $this->render('JOPlatformBundle:Advert:add.html.twig');
+	}
+
+	public function editAction($id, Request $request)
+	{
+		if ($request->isMethod('POST'))
+		{
+			$request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifée');
+			
+			return $this->redirect($this->generateUrl('jo_platform_view', array('id'=>5)));
+		}
+		return $this->render('JOPlatformBundle:Advert:edit.html.twig');
+	}
+	
+	public function deleteAction($id)
+	{
+		return $this->render('JOPlatformBundle:Advert:delete.html.twig');
+	}
 	
 	public function byebyeAction()
 	{
