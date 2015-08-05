0412a6dc7411f2ec46f139c959c98163b6ca03b9
Joseph <joseph.mesny@gmail.com>
2015-08-05 14:58:43 +0200

    Add all view file with 3 step layout, add bootstap tweeter css on the website

--- src/JO/PlatformBundle/Controller/AdvertController.php
+++ src/JO/PlatformBundle/Controller/AdvertController.php
@@ -13,7 +13,14 @@ class AdvertController extends Controller
 	// récupération de l'annonce correspondant a l'id
     public function viewAction($id)
 	{
-		return $this->render('JOPlatformBundle:Advert:view.html.twig', array('id' => $id));
+		$advert = array(
+		  'title'   => 'Recherche développpeur Symfony2',
+		  'id'      => $id,
+		  'author'  => 'Alexandre',
+		  'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+		  'date'    => new \Datetime()
+		);
+		return $this->render('JOPlatformBundle:Advert:view.html.twig', array('advert'=>$advert));
 	}
 	
 	// On récupère tous les paramètres en arguments de la méthode
@@ -28,9 +35,32 @@ class AdvertController extends Controller
         // throw error in case of bad page number.
 		if ($page <1)
 			throw new NotFoundHttpException('Page "'.$page.'" inexistante.');
-		
+
+
+		// Notre liste d'annonce en dur
+		$listAdverts = array(
+		  array(
+			'title'   => 'Recherche développpeur Symfony2',
+			'id'      => 1,
+			'author'  => 'Alexandre',
+			'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+			'date'    => new \Datetime()),
+		  array(
+			'title'   => 'Mission de webmaster',
+			'id'      => 2,
+			'author'  => 'Hugo',
+			'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
+			'date'    => new \Datetime()),
+		  array(
+			'title'   => 'Offre de stage webdesigner',
+			'id'      => 3,
+			'author'  => 'Mathieu',
+			'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
+			'date'    => new \Datetime())
+		);
+
 		// TODO Return announce list
-		return $this->render('JOPlatformBundle:Advert:index.html.twig');
+		return $this->render('JOPlatformBundle:Advert:index.html.twig', array('listAdverts'=> $listAdverts));//array()));
     }
 	
 	// gestion du formulaire d'ajout d'annonces
@@ -40,7 +70,14 @@ class AdvertController extends Controller
 			$request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
 			return $this->redirect($this->generateUrl('jo_platform_view', array('id' => 5)));
 		}
-		return $this->render('JOPlatformBundle:Advert:add.html.twig');
+		$advert = array(
+			'title'   => 'Recherche développpeur Symfony2',
+			'id'      => 2,
+			'author'  => 'Alexandre',
+			'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+			'date'    => new \Datetime());
+
+		return $this->render('JOPlatformBundle:Advert:add.html.twig', array('advert'=> $advert));
 	}
 
 	public function editAction($id, Request $request)
@@ -51,17 +88,48 @@ class AdvertController extends Controller
 			
 			return $this->redirect($this->generateUrl('jo_platform_view', array('id'=>5)));
 		}
-		return $this->render('JOPlatformBundle:Advert:edit.html.twig');
+		
+		$advert = array(
+			'title'   => 'Recherche développpeur Symfony2',
+			'id'      => $id,
+			'author'  => 'Alexandre',
+			'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+			'date'    => new \Datetime());
+			
+		return $this->render('JOPlatformBundle:Advert:edit.html.twig', array('advert'=>$advert));
 	}
 	
 	public function deleteAction($id)
 	{
-		return $this->render('JOPlatformBundle:Advert:delete.html.twig');
+		$advert = array(
+			'title'   => 'Recherche développpeur Symfony2',
+			'id'      => $id,
+			'author'  => 'Alexandre',
+			'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+			'date'    => new \Datetime());
+			
+		return $this->render('JOPlatformBundle:Advert:delete.html.twig', array('advert'=>$advert));
 	}
 	
 	public function byebyeAction()
 	{
-        $content = $this->get('templating')->render('JOPlatformBundle:Advert:index.html.twig', array('nom' => 'Joseph'));
+        $content = $this->get('templating')->render('JOPlatformBundle:Advert:index.html.twig', array('listAdverts'=> array()));
 		return new Response($content);
+	}
+	
+	public function menuAction($limit)
+	{
+		// On fixe en dur une liste ici, bien entendu par la suite
+		// on la récupérera depuis la BDD !
+		$listAdverts = array(
+			array('id' => 2, 'title' => 'Recherche développeur Symfony2'),
+			array('id' => 5, 'title' => 'Mission de webmaster'),
+			array('id' => 9, 'title' => 'Offre de stage webdesigner')
+		);
+
+		return $this->render('JOPlatformBundle:Advert:menu.html.twig', array(
+		// Tout l'intérêt est ici : le contrôleur passe
+		// les variables nécessaires au template !
+		'listAdverts' => $listAdverts));
 	}
 }
